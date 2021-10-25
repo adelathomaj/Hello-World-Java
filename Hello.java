e273e748fc1cda10b3860cb25ba407f35d8ff18a
Adela Thomaj Hoti <87237785+adelathomaj@users.noreply.github.com>
2021-10-25 17:34:32 +0200

    Add files via upload
    Practicing Java University

---
+++ Hello.java
@@ -0,0 +1,13 @@
+package package_01;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World! This is a test");
+		System.out.print("This is a second test");
+		System.out.print("This is a third test");
+		System.out.println("\n This is a fourth test");
+
+	}//main
+
+}//class
